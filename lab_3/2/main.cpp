160aee24e17fa908165699e1316954581b96ef9e
Nikolay Mikhaylov <sonny@milton.pro>
2019-09-09 20:18:52 +0300

    Add lab 3

---
+++ lab_3/2/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+    int number;
+    int count = 0;
+    std::cout << "Input a positibe number: ";
+    std::cin >> number;
+
+    for (int i = 1; i <= number; i++) {
+        int m = i;
+        int p = 0;
+        while (m > 0) {
+            int s = m % 10;
+            p = p * 10 + s;
+            m /= 10;
+        }
+        if (p == i) {
+           count += 1;
+        }
+    }
+
+    std::cout << "There is " << count << " palindrom(s) in 1.." << number << " range" << std::endl;
+}
