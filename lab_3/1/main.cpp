c7f6cb96da3c72fa25f384e3f832b8513a6b0bee
Nikolay Mikhaylov <sonny@milton.pro>
2019-10-02 19:54:03 +0300

    Fix todo in lab 3.1

--- lab_3/1/main.cpp
+++ lab_3/1/main.cpp
@@ -1,1 +1,13 @@
-// TODO
+#include <iostream>
+#include <cmath>
+
+int main() {
+    float x0, deltaX;
+    int n;
+    std::cout << "Enter x0, delta x, and n:" << std::endl;
+    std::cin >> x0 >> deltaX >> n;
+    float x = x0;
+    for (int i=0; i < n; i++, x += deltaX) {
+        std::cout << "f(" << x << ") = " << 12.4 / (sqrt(x) - x * x) << std::endl; 
+    } 
+}
