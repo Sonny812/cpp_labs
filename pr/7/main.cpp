d674b08c26de55786ba001b79d1469039a0f904a
Nikolay Mikhaylov <sonny@milton.pro>
2020-01-16 05:08:19 +0300

    Update pr 7. Add linked list usage example

---
+++ pr/7/main.cpp
@@ -0,0 +1,18 @@
+#include "LinkedList.hpp"
+
+int main()
+{
+    auto list = LinkedList::createFromText("Hello world!");
+    list->deleteNodeBySymbol('o');
+    
+    auto reversed = list->reverse();
+    reversed->output();
+
+    list->clear();
+    reversed->clear();
+    
+    auto length = list->length();
+
+    delete list;
+    delete reversed;
+}
